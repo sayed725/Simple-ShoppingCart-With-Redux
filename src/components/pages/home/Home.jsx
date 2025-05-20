43f65f8c9fbe7db354d6a19c7def9115b9a1a1d7
sayed725 <ssayed72533@gmail.com>
2025-05-20 15:17:18 +0600

    added product card

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ProductCard from './products/ProductCard';
 
 const Home = () => {
+
+    const products = useSelector( (state )=> state.products);
+
+    // console.log(products)
+
+
+
+
+
     return (
-        <div>
-            HOME
+        <div className='py-8'>
+        <div className='grid lg:grid-cols-3 grid-cols-1 gap-8'>
+            <div className='col-span-2'>
+                <div className='grid lg:grid-cols-2 gap-4 gap-y-8'>
+                  {
+                    products.length ? products.map((product, index) => (
+                      <ProductCard key={index} product={product}/>
+                    ))  : <p>No product Found!</p>
+                  }
+                </div>
+            </div>
+
+
+            <div className='col-span-1'>
+             <h2> AddProduct</h2>
+            </div>
         </div>
+    </div>
     );
 };
 
---
+++ src/components/pages/home/products/ProductCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const ProductCard = ({product}) => {
+
+
+
+     const {name, image, price} = product || {};
+
+
+      const handleAddToCart = () => {
+    
+    }
+    // console.log(product)
+
+
+
+    return (
+       <div className="card bg-white w-96 shadow-xl">
+      <figure>
+        <img
+          src={image}
+          alt="Shoes"
+          className="w-full h-48 object-cover"
+        />
+      </figure>
+      <div className="card-body text-gray-700">
+        <h2 className="card-title">{name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions items-center justify-end">
+            <p className="text-xl font-semibold">${price}</p>
+          <button onClick={handleAddToCart} className="btn btn-primary bg-indigo-600 text-white">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default ProductCard;
